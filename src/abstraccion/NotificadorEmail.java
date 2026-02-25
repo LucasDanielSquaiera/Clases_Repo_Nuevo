630eb285ae65636e8c4d4f9fcba56950e338184e
Lucas <lucasdanielsquaiera@gmail.com>
2026-02-25 21:00:26 +0100

    Arreglamos el constructor de clases hijas

--- src/abstraccion/NotificadorEmail.java
+++ src/abstraccion/NotificadorEmail.java
@@ -2,11 +2,13 @@ package abstraccion;
 
 public class NotificadorEmail extends Notificador {
 	
-	public NotificadorEmail(String mensaje) {
-		super(mensaje);
-	}
 	
 	
+	
+	public NotificadorEmail(String canal) {
+		super(canal);
+		}
+
 	@Override
 	void enviarMensaje(String texto) {
 		System.out.println("Este correo electrónico es un mensaje de " + texto);
--- src/abstraccion/NotificadorSMS.java
+++ src/abstraccion/NotificadorSMS.java
@@ -2,9 +2,10 @@ package abstraccion;
 
 public class NotificadorSMS extends Notificador {
 
-	public NotificadorSMS(String mensaje) {
-		super(mensaje);
-	}
+	public NotificadorSMS(String canal) {
+		super(canal);
+		}
+
 	@Override
 	void enviarMensaje(String texto) {
 		System.out.println("Este mensaje teleónico es " + texto);
